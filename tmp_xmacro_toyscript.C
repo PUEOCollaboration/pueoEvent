86aa80d4c2cb711a641f56577f708352c8a677b6
jason yao <yao.966@osu.edu>
2026-01-22 06:13:37 +0000

    and we have a tree!

--- tmp_xmacro_toyscript.C
+++ tmp_xmacro_toyscript.C
@@ -1,7 +1,18 @@
+#include <cstdint>
+#include "TTree.h"
+
 #define STRINGIFY(X) _STRINGIFY(X)
 #define _STRINGIFY(X) #X
 #define CONCAT(X,Y) _CONCAT(X,Y)
 #define _CONCAT(X,Y) X##Y
+
+// for cern root tree type annotation
+#define TYPETAG_uint16_t s
+#define TYPETAG_uint8_t b
+#define TYPETAG_uint32_t i
+#define TYPETAG_float F
+#define TYPETAG_bool O
+
 
 #define PUEO_NCHAN 208 // only temporary!!!!
 
@@ -20,12 +31,19 @@ X(uint32_t,        ONEDIM,         L2_mask)\
 X(uint32_t,        ONEDIM,         soft_trigger)\
 X(uint32_t,        ONEDIM,         pps_trigger)\
 X(uint32_t,        ONEDIM,         ext_trigger)\
-X(pueo_time_t,     ONEDIM,         readout_time_utc_secs)\
-X(pueo_waveform_t, [PUEO_NCHAN],   wfs)
+// X(pueo_time_t,     ONEDIM,         readout_time_utc_secs)\
+// X(pueo_waveform_t, [PUEO_NCHAN],   wfs)
 
 
 void tmp_xmacro_toyscript(){
-#define X(VARTYPE, VARDIM, VARNAME) VARTYPE VARNAME VARDIM; 
+
+  TTree * fwft = new TTree("fullwaveform", "fullwaveform");
+
+#define X(VARTYPE, VARDIM, VARNAME) \
+VARTYPE VARNAME VARDIM = {};\
+fwft->Branch(#VARNAME, &VARNAME, STRINGIFY(VARNAME) STRINGIFY(VARDIM) "/" STRINGIFY(CONCAT(TYPETAG_,VARTYPE)));
 FULLWAVEFORM_FIELDS
 #undef X
+
+  fwft->Print();
 }
