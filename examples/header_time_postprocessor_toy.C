8a9bac365bf50b8522fc943421d8f885e6615ed7
jason yao <yao.966@osu.edu>
2026-02-12 22:20:10 +0000

    unwrap `UInt_32` to `UInt_64`

--- examples/header_time_postprocessor_toy.C
+++ examples/header_time_postprocessor_toy.C
@@ -7,29 +7,53 @@
 using ROOT::RDataFrame;
 using ROOT::RDF::RResultPtr;
 
-UInt_t rollover_difference_32bit(UInt_t big, UInt_t small){
+UInt_t rollover_difference_32bit(UInt_t big, UInt_t small)
+{
   return big > small ? big-small : big + UINT32_MAX-small;
 }
 
-struct pps_and_their_diff {
+struct pps_and_their_diff 
+{
   UInt_t last_pps = 0;
   UInt_t llast_pps = 0;
   UInt_t delta = 0; // last_pps - llast_pps, with rollover taken care of
 };
 
-// arbitrarily deciding that any diff larger than three clock count is "bad"
-constexpr UInt_t CLOCK_COUNT_TOLERANCE = 3;
+// "promotes" the Y-value range of the graph from [0, UINT32_MAX) to [0, UINT64_MAX)
+// warning: the first two seconds of the graph MUST have been removed as they contain garbage,
+// and this function is not smart enough about that 
+// (it only uses a slope to determine when a wrap-around has occured)
+void unwrap(TGraph * gr) 
+{
+  // some arbitrary negative number to determine whehter a wrap-around has occured.
+  // this number should be lenient enough -- that is, not exactly (-UINT32_MAX)
+  // that said, how lenient is lenient seems to be somewhat arbitrary...
+  const double SLOPE_TOLERANCE = -(double) UINT32_MAX / 5;
+
+  int num_wraps = 0;
+  for (int i=1; i < gr->GetN(); ++i)
+  {
+    double dy = (gr->GetPointY(i) + (double) UINT32_MAX * num_wraps) - gr->GetPointY(i-1);
+
+    if (dy < SLOPE_TOLERANCE) 
+    {
+      num_wraps++;
+    }
+    gr->SetPointY(i, gr->GetPointY(i) + (double) num_wraps * UINT32_MAX);
+  }
+}
 
 // Some assumptions about the data are made:
 // (a)  The column `event_second` is monotonically increasing, ie "sorted"
 // (b)  0 <= event_second[x] - event_second[x-1] <= 1
-void header_time_postprocessor_toy(){
-
+void header_time_postprocessor_toy()
+{
   gSystem->Load("libpueoEvent.so");
 
   // default already disables this so no need to explicitly disable
   // ROOT::DisableImplicitMT(); // can't multithread cuz of the lambda capture
-  RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/bfmr_r739_head.root");
+  // RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/bfmr_r739_head.root");
+  RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/real_R0813_head.root");
 
   std::map<UInt_t, pps_and_their_diff> encounters; // stores unique encounters of `event_second`
   TGraph evtsec_vs_lpps; // for interpolating/extrapolating last_pps at certain "bad" seconds
@@ -52,24 +76,26 @@ void header_time_postprocessor_toy(){
     };
   tmp_header_rdf.Foreach(search_and_fill, {"triggerTime","lastPPS","lastLastPPS"});
 
-  // ie. skip first two seconds when computing average
+  // remove first two seconds in the TGraph before unwrapping
+  evtsec_vs_lpps.RemovePoint(0);
+  evtsec_vs_lpps.RemovePoint(0);
+  unwrap(&evtsec_vs_lpps);
+
+  //  then re-evaluate with extrapolation
+  UInt_t first_second = encounters.begin()->first;
+  UInt_t second_second = std::next(encounters.begin(),1)->first;                 // + UINT32_MAX in case extrapolation is negative
+  encounters[first_second].last_pps = (ULong64_t)(evtsec_vs_lpps.Eval(first_second) + UINT32_MAX) % UINT32_MAX;
+  encounters[second_second].last_pps = (ULong64_t)(evtsec_vs_lpps.Eval(second_second) + UINT32_MAX) % UINT32_MAX;
+
+                                   // ie. skip first two seconds when computing average delta
   deltas = ROOT::VecOps::Take(deltas, -deltas.size() + 2);
   double avg_delta = ROOT::VecOps::Mean(deltas);
 
-  UInt_t first_second = encounters.begin()->first;
-  UInt_t second_second = std::next(encounters.begin(),1)->first;
-  // remove first two seconds in the TGraph, and then re-evaluate with extrapolation
-  evtsec_vs_lpps.RemovePoint(0);
-  evtsec_vs_lpps.RemovePoint(0);
-  encounters[first_second].last_pps = (ULong64_t)(evtsec_vs_lpps.Eval(first_second) + UINT32_MAX) % UINT32_MAX;
-  encounters[second_second].last_pps = (ULong64_t)(evtsec_vs_lpps.Eval(second_second) + UINT32_MAX) % UINT32_MAX;
-  std::cout << encounters[first_second].last_pps << std::endl;;
-  std::cout << encounters[second_second].last_pps;
 
-  // TCanvas c1("", "", 1920, 1080);
-  // evtsec_vs_lpps.Draw("ALP");
-  // evtsec_vs_lpps.SetMarkerStyle(kCircle);
+  TCanvas c1("", "", 1920, 1080);
+  evtsec_vs_lpps.Draw("ALP");
+  evtsec_vs_lpps.SetMarkerStyle(kCircle);
   // evtsec_vs_lpps.GetXaxis()->SetRangeUser(evtsec_vs_lpps.GetPointX(0), evtsec_vs_lpps.GetPointX(0)+10);
-  // c1.SaveAs("foo.svg");
+  c1.SaveAs("bar.svg");
   exit(0);
 }
--- examples/print_unique_seconds_and_plot_delta.C
+++ examples/print_unique_seconds_and_plot_delta.C
@@ -11,12 +11,13 @@ UInt_t rollover_difference_32bit(UInt_t big, UInt_t small){
   return big > small ? big-small : big + UINT32_MAX-small;
 }
 
-void print_unique_seconds(){
+void print_unique_seconds_and_plot_delta(){
 
   gSystem->Load("libpueoEvent.so");
 
   // ROOT::DisableImplicitMT(); // default already disables this so no need
-  RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/real_R0813_head.root");
+  // RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/real_R0813_head.root");
+  RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/bfmr_r739_head.root");
 
   std::unordered_set<UInt_t> encounters;
   UInt_t event_second, last_pps, llast_pps, delta;
@@ -48,7 +49,7 @@ void print_unique_seconds(){
   RDataFrame unique_rdf(unique_trigger_time);
   auto delta_gr = unique_rdf.Range(2,0).Graph<UInt_t, UInt_t>("event_second", "delta");
 
-  unique_rdf.Display({"event_second", "last_pps", "llast_pps", "delta"}, 100)->Print();
+  unique_rdf.Display({"event_second", "last_pps", "llast_pps", "delta"}, 500)->Print();
   TCanvas c1("", "", 1920, 1080);
   delta_gr->Draw("ALP");
   delta_gr->SetMarkerStyle(kCircle);
