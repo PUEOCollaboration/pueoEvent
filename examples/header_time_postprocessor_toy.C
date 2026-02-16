948c452d452017aad4dfc7903a2546b1bc8e9e4d
jason yao <yao.966@osu.edu>
2026-02-16 21:32:15 +0000

    adding stupid extrapolation

--- examples/header_time_postprocessor_toy.C
+++ examples/header_time_postprocessor_toy.C
@@ -1,5 +1,6 @@
 #include "ROOT/RDataFrame.hxx"
 #include "TSystem.h"
+#include <climits>
 #include <iostream>
 #include <iomanip>
 #include <map>
@@ -8,23 +9,28 @@ using ROOT::RDataFrame;
 
 struct second_boundaries 
 {
-  UInt_t start = 0;
-  UInt_t end = 0;
-  UInt_t delta = 0; // last_pps - llast_pps, with rollover taken care of
+  UInt_t original_start = 0; // value of the sysclk counter at the start of the second
+  UInt_t original_end = 0;
+  UInt_t corrected_start = 0;
+  UInt_t corrected_end = 0;
+  UInt_t delta = 0; // end_pps - start_pps, with rollover taken care of
 };
 
-// approximately equal with some arbitrary default tolerance
-bool approx_equal(UInt_t a, UInt_t b, UInt_t tolerance = 200)
+// approximately equal with some arbitrary default tolerance (20 sysclk counts)
+bool approx_equal(UInt_t a, UInt_t b, UInt_t tolerance = 20)
 {
   UInt_t diff = a > b ? a - b : b - a;
   return diff <= tolerance;
 }
 
-void print(std::map<UInt_t, second_boundaries>& encounters);
+void print(std::map<Long64_t, second_boundaries>& utcSecond_start_end_delta_start_end);
 
-// average value of (end-start); last two seconds should be excluded when computing this,
-// because they don't have (start, end) pairs.
-UInt_t average_delta(std::map<UInt_t, second_boundaries>& encounters, UInt_t exclude1, UInt_t exclude2);
+// average value of (original_end-original_start); the final two seconds should be excluded when computing this,
+// because they don't have valid (original_start, original_end) pairs.
+UInt_t average_delta(std::map<Long64_t, second_boundaries>& encounters, Long64_t exclude1, Long64_t exclude2);
+
+// first attempt at correcting the start and end of each second via extrapolation
+void stupid_extrapolation(std::map<Long64_t, second_boundaries>& encounters, UInt_t avg_delta);
 
 // Some assumptions about the data are made:
 // (a)  The column `event_second` is monotonically increasing, ie "sorted"
@@ -35,61 +41,64 @@ void header_time_postprocessor_toy()
 
   // default already disables this so no need to explicitly disable
   // ROOT::DisableImplicitMT(); // can't multithread cuz of the lambda capture
-  RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/bfmr_r739_head.root");
-  // RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/real_R0813_head.root");
+  // RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/bfmr_r739_head.root");
+  RDataFrame tmp_header_rdf("header", "/usr/pueoBuilder/install/bin/real_R0813_head.root");
+  // the map stores unique encounters of `event_second` (aka triggerTime)
+  std::map<Long64_t, second_boundaries> encounters;
 
-  std::map<UInt_t, second_boundaries> encounters; // stores unique encounters of `event_second`
-
-  UInt_t previous_second   = UINT32_MAX;     // initialize to garbage
-  UInt_t previous_previous = UINT32_MAX - 1; // initialize to garbage
+  Long64_t previous_previous = LONG_LONG_MIN; // initialize to garbage
+  Long64_t previous_second   = LONG_LONG_MIN+1;
   auto search_and_fill = 
     [&encounters, &previous_second, &previous_previous]
     (UInt_t event_second, UInt_t lpps)
     {
-      bool new_encounter = encounters.find(event_second) == encounters.end();
+      Long64_t evtsec = (Long64_t) event_second;
+      bool new_encounter = encounters.find(evtsec) == encounters.end();
       if (new_encounter) 
       {
-        encounters.emplace(event_second, second_boundaries{});
+        encounters.emplace(evtsec, second_boundaries{});
         // the start of the previous second is the lpps of the current second
         // note that this inserts garbage into `encounters` during the first two iterations;
-        encounters[previous_second].start = lpps;
-        encounters[previous_previous].end = lpps;
-        encounters[previous_previous].delta = lpps - encounters[previous_previous].start;
+        encounters[previous_second].original_start = lpps;
+        encounters[previous_previous].original_end = lpps;
+        encounters[previous_previous].delta = lpps - encounters[previous_previous].original_start;
         // note: wrap-around subtraction is automatic for unsigned integers
         
         previous_previous = previous_second;
-        previous_second = event_second;
+        previous_second = evtsec;
       }
     };
   tmp_header_rdf.Foreach(search_and_fill, {"triggerTime","lastPPS"});
-  encounters.erase(UINT32_MAX);  // erase the garbage
-  encounters.erase(UINT32_MAX-1);
-  print(encounters);
+  encounters.erase(LONG_LONG_MIN);  // erase the garbage
+  encounters.erase(LONG_LONG_MIN+1);
+  // print(encounters);
 
-  // UInt_t avg_delta = average_delta(encounters, previous_second, previous_previous);
-  //
-  // // iterate from 1st entry to 2nd-to-last entry
-  // for (auto it = encounters.begin(); it != std::prev(encounters.end()); it++) 
-  // {
-  //
-  // }
+  UInt_t avg_delta = average_delta(encounters, previous_second, previous_previous);
+  stupid_extrapolation(encounters, avg_delta);
+  // print(encounters);
+
   exit(0);
 }
 
-void print(std::map<UInt_t, second_boundaries>& encounters)
+void print(std::map<Long64_t, second_boundaries>& encounters)
 {
-  std::cout << "   second   |   start   |    end    |  delta" << "\n"
-            << "--------------------------------------------\n";
+
+  std::cout << "-------------------------------------------------------------------------------\n"
+            << "            |   original                        |    corrected                 \n"
+            << "   second   |   start   |    end    |  delta    |    start   |    end    " << "\n"
+            << "-------------------------------------------------------------------------------\n";
   for (auto& e: encounters)
   {
     std::cout << std::setw(11) << e.first
-              << " " << std::setw(11) << e.second.start
-              << " " << std::setw(11) << e.second.end
-              << " " << std::setw(11) << e.second.delta << "\n";
+              << " " << std::setw(11) << e.second.original_start
+              << " " << std::setw(11) << e.second.original_end
+              << " " << std::setw(11) << e.second.delta
+              << " " << std::setw(11) << e.second.corrected_start
+              << " " << std::setw(11) << e.second.corrected_end << "\n";
   }
 }
 
-UInt_t average_delta(std::map<UInt_t, second_boundaries>& encounters, UInt_t exclude1, UInt_t exclude2)
+UInt_t average_delta(std::map<Long64_t, second_boundaries>& encounters, Long64_t exclude1, Long64_t exclude2)
 {
   ULong64_t sum = 0;
   for (auto& e: encounters) sum += e.second.delta;
@@ -97,4 +106,52 @@ UInt_t average_delta(std::map<UInt_t, second_boundaries>& encounters, UInt_t exc
   sum -= encounters[exclude2].delta;
   sum /= encounters.size()-2;
   return sum;
+}
+
+void stupid_extrapolation(std::map<Long64_t, second_boundaries>& encounters, UInt_t avg_delta)
+{
+  // find the mid-point of a "stable region" where the delta's are all approximately avg_delta
+  std::size_t stable_period = encounters.size() / 3;
+  auto mid_point = encounters.begin();
+  std::vector<ULong64_t> stable_seconds;
+  stable_seconds.reserve(stable_period);
+
+  for (auto &e: encounters) 
+  {
+    if (stable_seconds.size() == stable_period) break; 
+
+    if (approx_equal(e.second.delta , avg_delta)) stable_seconds.emplace_back(e.first);
+    else stable_seconds.clear();
+  }
+
+  if (stable_seconds.size() != stable_period) 
+  {
+    print(encounters);
+    fprintf(
+      stderr,
+      "\e[31;1mCouldn't find a stable region (required: %lu stable seconds) "
+      "where `end_pps` - `start_pps` are all approximately %u clock counts.\n"
+      "Falling back to the assumption that the first second (%llu) is a \"good second\"\n\e[31;0m",
+      stable_period, avg_delta, mid_point->first
+    );
+  } else {
+    mid_point = encounters.find(stable_seconds.at(stable_period/2));
+  }
+
+  for (auto it = encounters.begin(); it!=mid_point; ++it)
+  {
+    Long64_t diff_sec = it->first - mid_point->first;
+    it->second.corrected_start = (mid_point->second.original_start) + diff_sec * avg_delta;
+    it->second.corrected_end = (mid_point->second.original_end) + diff_sec * avg_delta;
+  }
+
+  mid_point->second.corrected_start = mid_point->second.original_start;
+  mid_point->second.corrected_end = mid_point->second.original_end;
+
+  for (auto it = std::next(mid_point); it!=encounters.end(); ++it)
+  {
+    Long64_t diff_sec = it->first - mid_point->first;
+    it->second.corrected_start = (mid_point->second.original_start) + diff_sec * avg_delta;
+    it->second.corrected_end = (mid_point->second.original_end) + diff_sec * avg_delta;
+  }
 }
