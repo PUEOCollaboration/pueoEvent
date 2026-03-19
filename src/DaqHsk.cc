d028b734ee39a7704a6da203c55f71c9945898b9
jason yao <yao.966@osu.edu>
2026-03-19 20:50:34 +0000

    same deal for DawHsk

--- src/DaqHsk.cc
+++ src/DaqHsk.cc
@@ -21,14 +21,7 @@
 *
 ****************************************************************************************/ 
 
-
-
 #include "pueo/DaqHsk.h" 
-
-ClassImp(pueo::daqhsk::DaqHsk); 
-ClassImp(pueo::daqhsk::Beam); 
-ClassImp(pueo::daqhsk::Surf); 
-
 #ifdef HAVE_PUEORAWDATA
 
 pueo::daqhsk::DaqHsk::DaqHsk(const pueo_daq_hsk_t *daqhsk) :
@@ -36,73 +29,73 @@ pueo::daqhsk::DaqHsk::DaqHsk(const pueo_daq_hsk_t *daqhsk) :
   l2_readout_timeNsecs(daqhsk->l2_readout_time.utc_nsecs),
   scaler_readout_time(daqhsk->scaler_readout_time.utc_secs),
   scaler_readout_timeNsecs(daqhsk->scaler_readout_time.utc_nsecs),
-    soft_rate(daqhsk->soft_rate),
-    pps_rate(daqhsk->pps_rate),
-    ext_rate(daqhsk->ext_rate),
-    MIE_total_H(daqhsk->MIE_total_H),
-    MIE_total_V(daqhsk->MIE_total_V),
-    LF_total_H(daqhsk->LF_total_H),
-    LF_total_V(daqhsk->LF_total_V),
-    aux_total(daqhsk->aux_total),
-    global_total(daqhsk->global_total),
-    l2_enable_mask(daqhsk->l2_enable_mask),
-    qwords_sent(daqhsk->qwords_sent),
-    events_sent(daqhsk->events_sent),
-    trigger_count(daqhsk->trigger_count),
-    current_second(daqhsk->current_second),
-    last_pps(daqhsk->last_pps),
-    llast_pps(daqhsk->llast_pps),
-    last_dead(daqhsk->last_dead),
-    llast_dead(daqhsk->llast_dead),
-    panic_count(daqhsk->panic_count),
-    occupancy(daqhsk->occupancy),
-    ack_count(daqhsk->ack_count),
-    latency(daqhsk->latency),
-    offset(daqhsk->offset),
-    pps_trig_offset(daqhsk->pps_trig_offset)
+  soft_rate(daqhsk->soft_rate),
+  pps_rate(daqhsk->pps_rate),
+  ext_rate(daqhsk->ext_rate),
+  MIE_total_H(daqhsk->MIE_total_H),
+  MIE_total_V(daqhsk->MIE_total_V),
+  LF_total_H(daqhsk->LF_total_H),
+  LF_total_V(daqhsk->LF_total_V),
+  aux_total(daqhsk->aux_total),
+  global_total(daqhsk->global_total),
+  l2_enable_mask(daqhsk->l2_enable_mask),
+  qwords_sent(daqhsk->qwords_sent),
+  events_sent(daqhsk->events_sent),
+  trigger_count(daqhsk->trigger_count),
+  current_second(daqhsk->current_second),
+  last_pps(daqhsk->last_pps),
+  llast_pps(daqhsk->llast_pps),
+  last_dead(daqhsk->last_dead),
+  llast_dead(daqhsk->llast_dead),
+  panic_count(daqhsk->panic_count),
+  occupancy(daqhsk->occupancy),
+  ack_count(daqhsk->ack_count),
+  latency(daqhsk->latency),
+  offset(daqhsk->offset),
+  pps_trig_offset(daqhsk->pps_trig_offset)
 {
-    for (size_t i = 0; i < H_scalers.size(); ++i)
+  for (size_t i = 0; i < H_scalers.size(); ++i)
+  {
+    H_scalers[i] = daqhsk->Hscalers[i];
+    V_scalers[i] = daqhsk->Vscalers[i];
+  }
+
+  for (size_t i = 0; i < turfio_L1_rate.size(); ++i)
+    for (size_t j = 0; j < turfio_L1_rate[i].size(); ++j)
+      turfio_L1_rate[i][j] = daqhsk->turfio_L1_rate[i][j];
+
+  for (size_t i = 0; i < turfio_words_recv.size(); ++i)
+    turfio_words_recv[i] = daqhsk->turfio_words_recv[i];
+
+  for (size_t s = 0; s < Surfs.size(); ++s)
+  {
+    const auto &src_surf = daqhsk->surfs[s];
+    auto &dst_surf = Surfs[s];
+
+    dst_surf.readoutTime = src_surf.readout_time_start.utc_secs;
+    dst_surf.readoutTimeNsecs = src_surf.readout_time_start.utc_nsecs;
+    dst_surf.ms_elapsed = src_surf.ms_elapsed;
+    dst_surf.surf_link = src_surf.surf_link;
+    dst_surf.surf_slot = src_surf.surf_slot;
+
+    for (size_t c = 0; c < dst_surf.agc_scale.size(); ++c)
     {
-        H_scalers[i] = daqhsk->Hscalers[i];
-        V_scalers[i] = daqhsk->Vscalers[i];
+      dst_surf.agc_scale[c] = src_surf.agc_scale[c];
+      dst_surf.agc_offset[c] = src_surf.agc_offset[c];
     }
-    
-    for (size_t i = 0; i < turfio_L1_rate.size(); ++i)
-        for (size_t j = 0; j < turfio_L1_rate[i].size(); ++j)
-        turfio_L1_rate[i][j] = daqhsk->turfio_L1_rate[i][j];
-    
-    for (size_t i = 0; i < turfio_words_recv.size(); ++i)
-        turfio_words_recv[i] = daqhsk->turfio_words_recv[i];
-    
-    for (size_t s = 0; s < Surfs.size(); ++s)
+
+    for (size_t b = 0; b < dst_surf.Beams.size(); ++b)
     {
-        const auto &src_surf = daqhsk->surfs[s];
-        auto &dst_surf = Surfs[s];
-    
-        dst_surf.readoutTime = src_surf.readout_time_start.utc_secs;
-        dst_surf.readoutTimeNsecs = src_surf.readout_time_start.utc_nsecs;
-        dst_surf.ms_elapsed = src_surf.ms_elapsed;
-        dst_surf.surf_link = src_surf.surf_link;
-        dst_surf.surf_slot = src_surf.surf_slot;
-    
-        for (size_t c = 0; c < dst_surf.agc_scale.size(); ++c)
-        {
-        dst_surf.agc_scale[c] = src_surf.agc_scale[c];
-        dst_surf.agc_offset[c] = src_surf.agc_offset[c];
-        }
-    
-        for (size_t b = 0; b < dst_surf.Beams.size(); ++b)
-        {
-        const auto &src_beam = src_surf.beams[b];
-        auto &dst_beam = dst_surf.Beams[b];
-        dst_beam.threshold = src_beam.threshold;
-        dst_beam.pseudothreshold = src_beam.pseudothreshold;
-        dst_beam.scaler = src_beam.scaler;
-        dst_beam.pseudoscaler = src_beam.pseudoscaler;
-        dst_beam.inMask = src_beam.in_mask;
-        dst_beam.scalerBank = src_beam.scaler_bank;
-        }
+      const auto &src_beam = src_surf.beams[b];
+      auto &dst_beam = dst_surf.Beams[b];
+      dst_beam.threshold = src_beam.threshold;
+      dst_beam.pseudothreshold = src_beam.pseudothreshold;
+      dst_beam.scaler = src_beam.scaler;
+      dst_beam.pseudoscaler = src_beam.pseudoscaler;
+      dst_beam.inMask = src_beam.in_mask;
+      dst_beam.scalerBank = src_beam.scaler_bank;
     }
+  }
 }
 
 #endif
--- src/pueo/DaqHsk.h
+++ src/pueo/DaqHsk.h
@@ -23,12 +23,10 @@
 *
 ****************************************************************************************/ 
 
-
 #ifndef PUEO_DAQHSK_H
 #define PUEO_DAQHSK_H
 
-//Includes
-#include <TObject.h>
+#include "Rtypes.h"
 #include "pueo/Conventions.h"
 #include <array>
 #ifdef HAVE_PUEORAWDATA
@@ -36,91 +34,79 @@
 #endif
 namespace pueo 
 {
+namespace daqhsk 
+{
+class Beam{
+public: 
+  Beam() {;}
+  UInt_t threshold=0;
+  UInt_t pseudothreshold=0;
+  UShort_t scaler=0;
+  UShort_t pseudoscaler=0;
+  Bool_t inMask=0;
+  Bool_t scalerBank=0;
+  ClassDefNV(Beam,1);
+};
+class Surf{
+public:
+  Surf() {;}
+  // where are the constants like PUEO_NSURF and PUEO_NBEAMS
+  std::array<UInt_t,8> agc_scale;// this should be PUEO_NCHAN_PER_SURF
+  std::array<UInt_t,8> agc_offset; // this should be PUEO_NCHAN_PER_SURF
+  UInt_t readoutTime = 0;
+  UInt_t readoutTimeNsecs = 0;
+  UInt_t ms_elapsed = 0;
+  uint8_t surf_link = 0;
+  uint8_t surf_slot = 0;
+  std::array<daqhsk::Beam,pueo::k::NUM_BEAMS> Beams; 
+  ClassDefNV(Surf,1);
+};
 
+// per class there will be a surf array that has the surf information and beam array inside it
 
-
-  namespace daqhsk 
-  {
-
-    class Beam:  public TObject{
-      public: 
-        Beam() {;}
-        UInt_t threshold=0;
-        UInt_t pseudothreshold=0;
-        UShort_t scaler=0;
-        UShort_t pseudoscaler=0;
-        Bool_t inMask=0;
-        Bool_t scalerBank=0;
-        ClassDef(Beam,1);
-    };
-    class Surf: public TObject{
-     public:
-       Surf() {;}
-       virtual ~Surf() {;}
-       // where are the constants like PUEO_NSURF and PUEO_NBEAMS
-       std::array<UInt_t,8> agc_scale;// this should be PUEO_NCHAN_PER_SURF
-       std::array<UInt_t,8> agc_offset; // this should be PUEO_NCHAN_PER_SURF
-       UInt_t readoutTime = 0;
-       UInt_t readoutTimeNsecs = 0;
-       UInt_t ms_elapsed = 0;
-       uint8_t surf_link = 0;
-       uint8_t surf_slot = 0;
-       std::array<daqhsk::Beam,pueo::k::NUM_BEAMS> Beams; 
-       ClassDef(Surf,1);
-    };
-
-
-    /** @defgroup rootclasses The ROOT Classes
-     * These are the ROOT clases that make up the event reader
-     */
-    // per class there will be a surf array that has the surf information and beam array inside it
-
-    // then totally different class for the trigger information and l2 readout time, etc
-    class DaqHsk: public TObject
-    {
-     public:
-       DaqHsk() {;}
+// then totally different class for the trigger information and l2 readout time, etc
+class DaqHsk{
+public:
+  DaqHsk() {;}
 #ifdef HAVE_PUEORAWDATA
-       DaqHsk(const pueo_daq_hsk_t *daqhsk);
+  DaqHsk(const pueo_daq_hsk_t *daqhsk);
 #endif
-       virtual ~DaqHsk(){;}
-       UInt_t l2_readout_time = 0;
-       UInt_t l2_readout_timeNsecs = 0;
-       std::array<UInt_t,12> H_scalers;
-       std::array<UInt_t,12> V_scalers;
-       UInt_t scaler_readout_time = 0;
-       UInt_t scaler_readout_timeNsecs = 0;
-       std::array<std::array<UInt_t,7>,4> turfio_L1_rate;
-       UInt_t soft_rate = 0;
-       UInt_t pps_rate = 0;
-       UInt_t ext_rate = 0;
-       UShort_t MIE_total_H = 0;
-       UShort_t MIE_total_V = 0;
-       UShort_t LF_total_H = 0;
-       UShort_t LF_total_V = 0;
-       UShort_t aux_total = 0;
-       UShort_t global_total = 0;
-       UInt_t l2_enable_mask = 0;
-       std::array<UInt_t,4> turfio_words_recv;
-       UInt_t qwords_sent = 0;
-       UInt_t events_sent = 0;
-       UInt_t trigger_count = 0;
-       UInt_t current_second = 0;
-       UInt_t last_pps = 0;
-       UInt_t llast_pps = 0;
-       UInt_t last_dead = 0;
-       UInt_t llast_dead = 0;
-       UInt_t panic_count = 0;
-       UInt_t occupancy = 0;
-       UShort_t ack_count = 0;
-       UShort_t latency = 0;
-       UShort_t offset = 0;
-       UShort_t pps_trig_offset = 0;
-       std::array<daqhsk::Surf, pueo::k::NUM_SURF_SLOTS> Surfs;
-      ClassDef(DaqHsk,1);
-    };
-  }
+  UInt_t l2_readout_time = 0;
+  UInt_t l2_readout_timeNsecs = 0;
+  std::array<UInt_t,12> H_scalers;
+  std::array<UInt_t,12> V_scalers;
+  UInt_t scaler_readout_time = 0;
+  UInt_t scaler_readout_timeNsecs = 0;
+  std::array<std::array<UInt_t,7>,4> turfio_L1_rate;
+  UInt_t soft_rate = 0;
+  UInt_t pps_rate = 0;
+  UInt_t ext_rate = 0;
+  UShort_t MIE_total_H = 0;
+  UShort_t MIE_total_V = 0;
+  UShort_t LF_total_H = 0;
+  UShort_t LF_total_V = 0;
+  UShort_t aux_total = 0;
+  UShort_t global_total = 0;
+  UInt_t l2_enable_mask = 0;
+  std::array<UInt_t,4> turfio_words_recv;
+  UInt_t qwords_sent = 0;
+  UInt_t events_sent = 0;
+  UInt_t trigger_count = 0;
+  UInt_t current_second = 0;
+  UInt_t last_pps = 0;
+  UInt_t llast_pps = 0;
+  UInt_t last_dead = 0;
+  UInt_t llast_dead = 0;
+  UInt_t panic_count = 0;
+  UInt_t occupancy = 0;
+  UShort_t ack_count = 0;
+  UShort_t latency = 0;
+  UShort_t offset = 0;
+  UShort_t pps_trig_offset = 0;
+  std::array<daqhsk::Surf, pueo::k::NUM_SURF_SLOTS> Surfs;
+  ClassDefNV(DaqHsk,1);
+};
 }
-
+}
 
 #endif
