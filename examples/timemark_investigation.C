86c71d0111de7c7ad57b8abf416d5982ca15d0c3
jason yao <yao.966@osu.edu>
2026-03-31 20:03:02 +0000

    Christmas clock drift plot

--- examples/timemark_investigation.C
+++ examples/timemark_investigation.C
@@ -11,7 +11,23 @@ void timemark_investigation()
   gSystem->Load("libpueoEvent.so");
   ROOT::RDataFrame rdf("timemarkTree", "/work/all_timemarks.root");
 
-  rdf.Filter("rising.utc_secs >= 1767235954-5 && rising.utc_secs <= 1767235954+5")
-     .Display({"readout_time.utc_secs","rising.utc_secs", "rising.utc_nsecs"}, 100)->Print();
+  // rdf.Filter("rising.utc_secs >= 1767235954-5 && rising.utc_secs <= 1767235954+5")
+  //    .Display({"readout_time.utc_secs","rising.utc_secs", "rising.utc_nsecs"}, 100)->Print();
+
+  auto diff_bw = [](ULong64_t readout_sec, ULong64_t turf_sec)
+    {return static_cast<Long64_t>(readout_sec) - static_cast<Long64_t>(turf_sec);};
+  auto diff_df = rdf.Define("difference", diff_bw, {"readout_time.utc_secs", "rising.utc_secs"});
+
+  auto gr = diff_df.Graph<ULong64_t, Long64_t>("rising.utc_secs", "difference");
+  TCanvas c1("","", 1920, 1080);  
+  gr->SetTitle("timemark_t.readout_time.utc_secs - timemark_t.rising.utc_secs");
+  gr->GetYaxis()->CenterTitle();
+  gr->GetYaxis()->SetTitle("Difference [sec]");
+  gr->GetXaxis()->SetTitle("Date");
+  gr->GetXaxis()->SetRangeUser(1766.5e6, 1767e6); // seems like there's a clock drift during Christmas
+  gr->GetXaxis()->SetTimeDisplay(1);
+  gr->GetXaxis()->SetTimeFormat("%m/%d/%Y%F1970-01-01 00:00:00");
+  gr->Draw("ALP");
+  c1.SaveAs("timemark_investigation.png");
   
 }
