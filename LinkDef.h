87ff4d5fa0198eb7d5546874595b29df804079fb
jason yao <yao.966@osu.edu>
2026-04-01 18:14:54 +0000

    LinkDef schema evolution

--- LinkDef.h
+++ LinkDef.h
@@ -62,6 +62,97 @@
   source = " std::vector<double> volts[208]; "\
   code = "{ int iin = 0; for (int i = 0; i < 208; i++) { std::copy(onfile.volts[iin].begin(), onfile.volts[iin].end(), volts[i].begin()); iin++; }}"
 
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "run" \
+  version =  "[2]" \
+  source = "Int_t run;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "event_second" \
+  version =  "[2]" \
+  source = "UInt_t triggerTime;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "event_second" \
+  version =  "[2]" \
+  source = "UInt_t triggerTime;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "readout_time" \
+  version =  "[2]" \
+  source = "UInt_t readoutTime; UInt_t readoutTimeNs;"\
+  code="{readout_time = TTimeStamp((time_t) onfile.readoutTime, onfile.readoutTimeNs);}"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "event_number" \
+  version =  "[2]" \
+  source = "ULong_t eventNumber;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "L2_mask" \
+  version =  "[2]" \
+  source = "UInt_t L2Mask;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "trig_type" \
+  version =  "[2]" \
+  source = "UInt_t trigType;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "event_time" \
+  version =  "[2]" \
+  source = "UInt_t trigTime;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "last_pps" \
+  version =  "[2]" \
+  source = "UInt_t lastPPS;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "llast_pps" \
+  version =  "[2]" \
+  source = "UInt_t lastLastPPS;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "deadtime_counter" \
+  version =  "[2]" \
+  source = "UShort_T deadTime;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "deadtime_counter_last_pps" \
+  version =  "[2]" \
+  source = "UShort_T deadTimeLastPPS;"
+
+#pragma read \
+  targetClass = "pueo::RawHeader"\
+  sourceClass = "pueo::RawHeader"\
+  target = "deadtime_counter_llast_pps" \
+  version =  "[2]" \
+  source = "UShort_T deadTimeLastLastPPS;"
 
 #else
 #error "for compilation"
