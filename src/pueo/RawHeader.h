7cc3cf464036b4a00b6b706106457d8880f07291
jason yao <yao.966@osu.edu>
2026-04-01 01:34:27 +0000

    make `RawHeader.h` similar to `raw_data.h`

--- src/pueo/RawHeader.h
+++ src/pueo/RawHeader.h
@@ -23,9 +23,10 @@
 #ifndef PUEO_RAW_HEADER_H
 #define PUEO_RAW_HEADER_H
 
-//Includes
-#include "Rtypes.h"
 #include "pueo/Conventions.h"
+#include "TTimeStamp.h"
+#include "Rtypes.h"
+#include <cstdint>
 
 #ifdef HAVE_PUEORAWDATA
 #include "pueo/rawdata.h"
@@ -38,44 +39,57 @@
 
 namespace pueo
 {
-  class RawHeader
-  {
-     public:
-     RawHeader() {;} ///< Default constructor
+class RawHeader
+{
+public:
+  RawHeader():readout_time(0,0),corrected_trigger_time(0,0){;} ///< Default constructor
 
 #ifdef HAVE_PUEORAWDATA
-     RawHeader(const pueo_full_waveforms_t * wfs);
+  RawHeader(const pueo_full_waveforms_t * wfs);
 #endif
-     Int_t           run = 0 ; ///< Run number, assigned on ground
-     UInt_t          triggerTime= 0 ; ///< from the DAQ, may be correct or not
-     UInt_t          triggerTimeNs= 0;
-     UInt_t          readoutTime = 0 ; ///< unixTime of readout
-     UInt_t          readoutTimeNs= 0 ; ///< sub second time of readout
-     ULong_t         eventNumber = 0 ; ///< Software event number
-     UInt_t          L2Mask = {0};
-     UInt_t          phiTrigMask[k::NUM_POLS] = {0}; ///< 24-bit phi mask (from TURF)
-     UInt_t          flags = 0; 
+  int32_t   run = -999; ///< Run number, assigned on ground
+  int32_t   event_number = -999;
 
+  /* An example:
 
-     UInt_t        trigType = 0; /// set pueo::trigger namespace in Conventions
-     UInt_t        trigTime = 0; ///< Trigger time in TURF clock ticks
-     UInt_t        lastPPS = 0; ///< Number of TURF clock ticks between GPS pulse per seconds
-     UInt_t        lastLastPPS = 0; ///< Number of TURF clock ticks between GPS pulse per seconds
+            `last_pps`-------+       +-------- `event_time`
+                             ↓       ↓      
+     0--------125E6--------250E6-----o--375E6--------500E6--------625E6-------->
+     |                               |                                    [TURF clock ticks]
+  Run Start                       trigger!
+     |                               |                                    [sec]
+    111--------112----------113----------114----------115----------116---------->
+                             ↑
+          `event_second` ----+        
 
+  */
+  int32_t    event_second = 0; ///< Originally a uint32_t in libpueorawdata but int32_t is okay before year 2038
+                               ///< Value can be wrong prior to post-processing.
 
-     UShort_t        deadTime = 0;
-     UShort_t        deadTimeLastPPS = 0;
-     UShort_t        deadTimeLastLastPPS = 0;
+  uint32_t   event_time = 0; ///< 32-bit free running TURF clock value very briefly after trigger.
+  uint32_t   last_pps = 0; ///< TURF clock value at `event_second`. Value can be wrong prior to post-processing.
+  uint32_t   llast_pps = 0; ///< ... one second prior, value can be wrong, won't be corrected
+  uint32_t   deadtime_counter = 0; ///< @todo: no idea what these are, need help documenting
+  uint32_t   deadtime_counter_last_pps = 0;
+  uint32_t   deadtime_counter_llast_pps = 0;
+  uint32_t   L2_mask = 0;
+  uint32_t   trig_type = 0; ///< soft, pps, or ext trigger (see also pueo::trigger in Conventions.h)
+  TTimeStamp readout_time; ///< Time since Unix epoch [sec]
+                           ///< Tagged by the flight computer upon packet creation (ie event reception)
 
-     UChar_t         L1Octants[k::NUM_SURF_SLOTS] ={0};
+  TTimeStamp corrected_trigger_time; ///< correction via post-processing with pueo::Timemark
+                                     ///< second: from (corrected) `event_second`
+                                     ///< nanosecond: from `event_time` and (corrected) `last_pps`
 
-     // Trigger info
-     int isInPhiMask(int phi, pol::pol_t=pol::kVertical) const; ///< Returns 1 if given phi-pol is in mask
+  // uint32_t  flags = 0; @todo: not popuated
+  // uint32_t  phi_trigMask[k::NUM_POLS] = {0}; ///< 24-bit phi mask (from TURF) @todo: not populated
+  // UChar_t   L1Octants[k::NUM_SURF_SLOTS] ={0}; @todo not populated
 
+  // Trigger info
+  // int isInPhiMask(int phi, pol::pol_t=pol::kVertical) const; ///< Returns 1 if given phi-pol is in mask
 
-    ClassDefNV(RawHeader,3);
-
-  };
+  ClassDefNV(RawHeader,4);
+};
 }
 
 
