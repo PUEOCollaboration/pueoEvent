92b083da1d78cabb0b55b7bc89255b1f8652416a
jason yao <yao.966@osu.edu>
2026-04-03 20:30:35 +0000

    timemark clock drift plot: new timemark version

--- examples/timemark_investigation.C
+++ examples/timemark_investigation.C
@@ -2,6 +2,7 @@
 // This is not an example, but I need somewhere to store this script so here it is.
 //
 
+#include "TTimeStamp.h"
 #include "TSystem.h"
 #include "ROOT/RDataFrame.hxx"
 #include "TCanvas.h"
@@ -14,17 +15,18 @@ void timemark_investigation()
   // rdf.Filter("rising.utc_secs >= 1767235954-5 && rising.utc_secs <= 1767235954+5")
   //    .Display({"readout_time.utc_secs","rising.utc_secs", "rising.utc_nsecs"}, 100)->Print();
 
-  auto diff_bw = [](ULong64_t readout_sec, ULong64_t turf_sec)
-    {return static_cast<Long64_t>(readout_sec) - static_cast<Long64_t>(turf_sec);};
-  auto diff_df = rdf.Define("difference", diff_bw, {"readout_time.utc_secs", "rising.utc_secs"});
+  auto diff_bw = [](TTimeStamp& readout_sec, TTimeStamp& rising)
+    {return readout_sec.GetSec() - rising.GetSec();};
+  auto diff_df = rdf.Define("difference", diff_bw, {"readout_time", "rising"})
+                    .Define("x_axis", [](TTimeStamp& rising){return rising.GetSec();}, {"rising"});
 
-  auto gr = diff_df.Graph<ULong64_t, Long64_t>("rising.utc_secs", "difference");
+  auto gr = diff_df.Graph<time_t, time_t>("x_axis", "difference");
   TCanvas c1("","", 1920, 1080);  
   gr->SetTitle("timemark_t.readout_time.utc_secs - timemark_t.rising.utc_secs");
   gr->GetYaxis()->CenterTitle();
   gr->GetYaxis()->SetTitle("Difference [sec]");
   gr->GetXaxis()->SetTitle("Date");
-  gr->GetXaxis()->SetRangeUser(1766.5e6, 1767e6); // seems like there's a clock drift during Christmas
+  // gr->GetXaxis()->SetRangeUser(1766.5e6, 1767e6); // seems like there's a clock drift during Christmas
   gr->GetXaxis()->SetTimeDisplay(1);
   gr->GetXaxis()->SetTimeFormat("%m/%d/%Y%F1970-01-01 00:00:00");
   gr->Draw("ALP");
