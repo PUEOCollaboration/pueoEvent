2b43deb4222158c0067a32ed3ae590d74f05d198
jason <yao.966@osu.edu>
2026-04-20 14:49:36 -0400

    get shit working on OSC

--- scripts/pueo-time-table-SLURM.sh
+++ scripts/pueo-time-table-SLURM.sh
@@ -3,10 +3,19 @@
 #SBATCH -A PAS2608
 #SBATCH -t 2:30:00
 #SBATCH --ntasks-per-node=1
-#SBATCH --output=/fs/scratch/PAS2608/jason/global_timing_calibration/out/%a.out
-#SBATCH --error=/fs/scratch/PAS2608/jason/global_timing_calibration/out/%a.err
+
+#SBATCH --output=/dev/null
+#SBATCH --error=/dev/null
+#normally one could use %a to store the output of task number `a`, but we are remapping task numbers.
+
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=unmovingcastle
-
   
-root -l -q "${PARENT_DIR}/examples/header_time_postprocessor_toy.C(${SLURM_ARRAY_TASK_ID}, \"${TIMEMARK_ROOTFILE_PATH}\", \"${TIME_TABLE_PATH}\")"
+# A remap b/w run number and task id is needed because SLURM task id upper limit is 1000...
+run_number=$(( SLURM_ARRAY_TASK_ID + 700 ))
+
+# Have the standard output and standard error reflect the actual run number instead of task id
+exec > /fs/scratch/PAS2608/jason/global_timing_calibration/${run_number}.out
+exec 2>/fs/scratch/PAS2608/jason/global_timing_calibration/${run_number}.err
+
+root -l -q "${PARENT_DIR}/examples/header_time_postprocessor_toy.C(${run_number}, \"${TIMEMARK_ROOTFILE_PATH}\", \"${TIME_TABLE_PATH}\")"
--- scripts/pueo-time-table.sh
+++ scripts/pueo-time-table.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# @file pueo-time-table
+# @brief Submits a SLURM job array on OSC (each array job is one call to pueo-time-table-SLURM.sh)
+#        in order to create a "time table" for each run in PUEO_ROOT_DATA
+
 PARENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd) # absolute path of pueoEvent/
 TIME_TABLE_PATH=${HOME}/time_tables/                         # output path, structure is time_tables/run<run_number>/time_table.root
-TIMEMARK_ROOTFILE_PATH="/work/all_timemarks.root"            # path to all_timemarks.root
+TIMEMARK_ROOTFILE_PATH="/fs/ess/PAS0654/PUEO/flight/timemark.root"            # path to all_timemarks.root
 
 
 if [[ -z "$PUEO_ROOT_DATA" ]]; then
@@ -19,14 +23,21 @@ run_num_list=()
 for file in "${PUEO_ROOT_DATA}"/* ; do
   run=$(echo "$file" | sed -E 's/.*run([0-9]+)/\1/')
   mkdir -p ${TIME_TABLE_PATH}/run${run}
-  runs+=($run)
+  run_num_list+=($((run - 700)))  # very stupid, very dangerous, but SLURM id upperlimit is 1000
 done
 
 # convert to comma separated
-IFS=, run_num_csv="${runs[*]}"
+IFS=, run_num_csv="${run_num_list[*]}"
 unset IFS
 
-# use the run number as SLURM job array numbers
-sbatch --export=PARENT_DIR=${PARENT_DIR},TIMEMARK_ROOTFILE_PATH=${TIMEMARK_ROOTFILE_PATH},TIME_TABLE_PATH=${TIME_TABLE_PATH} \
+# store stdout/stderr log: 
+# this line is here because we're not using SLURM directives --output and --error inside the job script.
+rm -rf /fs/scratch/PAS2608/jason/global_timing_calibration/
+mkdir /fs/scratch/PAS2608/jason/global_timing_calibration/
+
+# use the run number as SLURM job array numbers; 
+# `--export=ALL` is needed to forward current environment, else the job will fail 
+# (will complain about CERN ROOT not found)
+sbatch --export=ALL,PARENT_DIR=${PARENT_DIR},TIMEMARK_ROOTFILE_PATH=${TIMEMARK_ROOTFILE_PATH},TIME_TABLE_PATH=${TIME_TABLE_PATH} \
        --array=${run_num_csv} \
        ${PARENT_DIR}/scripts/pueo-time-table-SLURM.sh
